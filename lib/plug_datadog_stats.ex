f455399f0de7f4f2e8a5a1f07f5279bf2b368aae
Pushkin Abbott <pabbott@pagerduty.com>
2018-09-07 16:12:14 -0400

    removing q/r

--- lib/plug_datadog_stats.ex
+++ lib/plug_datadog_stats.ex
@@ -48,7 +48,7 @@ defmodule PlugDatadogStats do
         "INT"
       String.match?(segment, ~r/^[a-f0-9]{8}(-[a-f0-9]{4}){3}-[a-f0-9]{12}$/)  ->
         "UUID"
-      String.match?(segment, ~r/(p|q|r)\w{6}/) ->
+      String.match?(segment, ~r/p\w{6}/) ->
         "OBFUSCATED_ID"
       true ->
         segment
